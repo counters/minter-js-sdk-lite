f9b0facb2596fd505da935e49a035c7c2b985f82
Webcounters <webcounters@gmail.com>
2020-06-30 17:45:12 +0300

    fix callback

--- src/MinterBalance.js
+++ src/MinterBalance.js
@@ -12,7 +12,6 @@ let MinterDefaultCoin='BIP';
         summ=0;
         numCoin=0;
         let oneBipMoney=false;
-        let maxVolume = 0;
 
         jQuery.each(array, function (key, value) {
             let valueF='';
@@ -34,15 +33,15 @@ let MinterDefaultCoin='BIP';
         if (oneBipMoney && numCoin===0 ) endCallback(summ);
     }
 
-    function getSumm(amound, coin, callback, endCallback=null) {
+    function getSumm(amound, coin, endCallback, callback=null) {
         MinterBalance.api( MinterBalance.api.SellCoin, amound, {'coin_to_sell':coin,'coin_to_buy':MinterDefaultCoin}, function (value) {
             numCoin--;
             let price=parseFloat(minterUtil.convertFromPip(value.result.will_get));
-            if ( endCallback!=null) endCallback(parseFloat(minterUtil.convertFromPip(amound)),coin,price);
+            if ( callback!=null) callback(parseFloat(minterUtil.convertFromPip(amound)),coin,price);
             summ+=price;
             if (summStr==='') summStr=minterUtil.convertFromPip(value.result.will_get) ; else
                 summStr=summStr+minterUtil.convertFromPip(value.result.will_get );
-            if (numCoin===0) callback(parseFloat(summStr));
+            if (numCoin===0) endCallback(parseFloat(summStr));
         });
     }
     return MinterBalance;
