ce8e92b744227db9c53bb10d7c8e2de6540c5502
Webcounters <webcounters@gmail.com>
2020-10-22 12:50:02 +0300

    last fix for API v1

--- src/MinterBalance.js
+++ src/MinterBalance.js
@@ -1,48 +1,65 @@
-
 let MinterBalance = (function () {
-let numCoin;
-let summ;
-let summStr='';
-let MinterDefaultCoin='BIP';
-    MinterBalance.api=null;
-/*    function parseFloat($strFloat) {
-        return($strFloat.float());
-    }*/
-    function MinterBalance(array, endCallback, callback=null) {
-        summ=0;
-        numCoin=0;
-        let oneBipMoney=false;
+    let numCoin;
+    let summ;
+    let summStr = '';
+    let MinterDefaultCoin = 'BIP';
+    MinterBalance.api = null;
+    let arrayCoin = {};
+    /*    function parseFloat($strFloat) {
+            return($strFloat.float());
+        }*/
+    function MinterBalance(array, endCallback, callback = null) {
+        summ = 0;
+        summStr = '';
+        numCoin = 0;
+        let oneBipMoney = false;
+        arrayCoin = [];
 
         jQuery.each(array, function (key, value) {
-            let valueF='';
-         let FeeValue=minterUtil.convertFromPip(value);
+            let valueF = '';
+            let FeeValue = minterUtil.convertFromPip(value);
 
-            if (key!==MinterDefaultCoin) {
+            if (key !== MinterDefaultCoin) {
                 numCoin++;
                 getSumm(value, key, endCallback, callback);
-                valueF= parseFloat(FeeValue);
+                valueF = parseFloat(FeeValue);
             } else {
-                if (summStr==='') summStr=FeeValue ;
-                let price=parseFloat(FeeValue);
-                summ+=price;
-                valueF= price;
-                oneBipMoney=true;
-                if ( callback!=null) callback(price,MinterDefaultCoin, price);
+                if (summStr === '') summStr = FeeValue;
+                let price = parseFloat(FeeValue);
+                summ += price;
+                valueF = price;
+                oneBipMoney = true;
+                const arr =  {amount: price, coin: MinterDefaultCoin, price: price};
+                arrayCoin.push(arr);
+                if (callback != null) callback(price, MinterDefaultCoin, price);
             }
         });
-        if (oneBipMoney && numCoin===0 ) endCallback(summ);
+        if (oneBipMoney && numCoin === 0) {
+            arrayCoin.sort(function(a, b){return b.price - a.price});
+            endCallback(summ, arrayCoin);
+        }
     }
 
-    function getSumm(amound, coin, endCallback, callback=null) {
-        MinterBalance.api( MinterBalance.api.SellCoin, amound, {'coin_to_sell':coin,'coin_to_buy':MinterDefaultCoin}, function (value) {
+    function getSumm(amount, coin, endCallback, callback = null) {
+        MinterBalance.api(MinterBalance.api.SellCoin, amount, {'coin_to_sell': coin, 'coin_to_buy': MinterDefaultCoin}, function (value) {
             numCoin--;
-            let price=parseFloat(minterUtil.convertFromPip(value.result.will_get));
-            if ( callback!=null) callback(parseFloat(minterUtil.convertFromPip(amound)),coin,price);
-            summ+=price;
-            if (summStr==='') summStr=minterUtil.convertFromPip(value.result.will_get) ; else
-                summStr=summStr+minterUtil.convertFromPip(value.result.will_get );
-            if (numCoin===0) endCallback(parseFloat(summStr));
+            let price = parseFloat(minterUtil.convertFromPip(value.result.will_get));
+            let amountNum =parseFloat(minterUtil.convertFromPip(amount));
+            if (callback != null) callback(amountNum, coin, price);
+            summ += price;
+            if (summStr === '') {
+                summStr = minterUtil.convertFromPip(value.result.will_get);
+            } else {
+                summStr = summStr + minterUtil.convertFromPip(value.result.will_get);
+            }
+            const _valueF = parseFloat(summStr);
+            arrayCoin.push({amount: amountNum, coin: coin, price: price});
+            if (numCoin === 0) {
+                arrayCoin.sort(function(a, b){return b.price - a.price});
+                endCallback(_valueF, arrayCoin);
+            }
         });
     }
+
     return MinterBalance;
 })();
